911f53fcc44ef72b1665f1ac7ccc9a758b3a9e78
Henry Chladil <s4293467@moss.labs.eait.uq.edu.au>
2015-06-25 09:59:21 +1000

    Fixed formatting

--- henry.c
+++ henry.c
@@ -6,68 +6,66 @@
 
 void error(int code) {
 
-	switch (code) {
-		case CTRLD:
-			fprintf(stderr, "Unexpected end of input.");
-			break;
-		case MEM:
-			fprintf(stderr, "Program out of memory.");
-			break;
-		case CWD:
-			fprintf(stderr, "Could not read current working directory.");
-			break;
-		default:
-			break;
-	}
-	exit(code);
+    switch (code) {
+	case CTRLD:
+	    fprintf(stderr, "Unexpected end of input.");
+	    break;
+	case MEM:
+	    fprintf(stderr, "Program out of memory.");
+	    break;
+	case CWD:
+	    fprintf(stderr, "Could not read current working directory.");
+	    break;
+	default:
+	    break;
+    }
+    exit(code);
 }
 
 
 char* copy_string(const char* str) {
-	char* tmp = malloc(strlen(str) + 1);
-	if (tmp == NULL) {
-		error(MEM);
-		return NULL;
-	} else {
-		return strcpy(tmp, str);
-	}
+    char* tmp = malloc(strlen(str) + 1);
+    if (tmp == NULL) {
+	error(MEM);
+	return NULL;
+    } else {
+	return strcpy(tmp, str);
+    }
 }
 
 
 char* handle_cwd(void) {
 
-	char cwd[1024];
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		return copy_string(cwd);
-	} else {
-		error(CWD);
-		return NULL;
-	}
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+	return copy_string(cwd);
+    } else {
+	error(CWD);
+	return NULL;
+    }
 }
 
 void parse_command(char* line) {
-
-	printf("%s\n", line);
+    printf("%s\n", line);
 }
 
 int main(int argc, char** argv) {
 
-	char line[255];
-	int c;
-	while (1) {
-		printf("%s$ ", handle_cwd());
-		if (fgets(line, sizeof(line), stdin) == NULL) {
-			error(CTRLD);
-		} else {
-			if (line[strlen(line)-1] != '\n') {
-				while ((c = getchar() != '\n') && (c != EOF)) {
-					// Expunge any remaining characters on stdin
-				}
-			} else {
-				line[strlen(line)-1] = '\0';
-			}
-			parse_command(copy_string(line));
-			//printf("%s\n", line);
+    char line[255];
+    int c;
+    while (1) {
+	printf("%s$ ", handle_cwd());
+	if (fgets(line, sizeof(line), stdin) == NULL) {
+	    error(CTRLD);
+	} else {
+	    if (line[strlen(line)-1] != '\n') {
+		while ((c = getchar() != '\n') && (c != EOF)) {
+				      // Expunge any remaining characters on stdin
 		}
+	    } else {
+		line[strlen(line)-1] = '\0';
+	    }
+	    parse_command(copy_string(line));
 	}
+    }
 }
